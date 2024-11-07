86ee17be1e19f51aaf272c7f17513928764d6580
webdevsk <72684302+webdevsk@users.noreply.github.com>
2024-11-07 22:09:38 +0600

    Added font

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import "./assets/InterVariable.ttf"
 import App from "./app/App.jsx"
 
 createRoot(document.getElementById("root")).render(
